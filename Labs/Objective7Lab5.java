2d798e274232ae4021f8c0bedb017f7b167620a4
Liz <lizbaze4730@gmail.com>
2022-09-11 14:15:09 -0500

    Objective7Lab5 complete

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+
+    int selection;
+
+    while (true) {
+      System.out.println("____Menu____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List my favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if(selection == 1) {
+        System.out.println("Hello World");
+      }
+      else if(selection == 2) {
+        System.out.println("Apple, Banana, Coconut");
+      }
+      else if(selection == 3) {
+        break;
+      }
+      else{
+        System.out.println("Invalid Selection");
+      }
+    }
+
+    scanner.close();
+  }
+}
